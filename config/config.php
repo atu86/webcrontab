2fd5924d20f112832877031d6a9e2b03a0625569
hxm <652120419@qq.com>
2016-01-05 14:38:57 +0800

    add del

--- config/config.php
+++ config/config.php
@@ -1,3 +1,1 @@
 test 1.1.1.1 user 22
-test2 2.2.2.2 user2 22
-test3 3.3.3.3 user 22
--- cron/del.php
+++ cron/del.php
@@ -3,8 +3,10 @@ $cfile='../config/config.php';
 $hid=$_POST[hid];
 $content = file_get_contents($cfile);
 $con_array = explode("\n", $content);
-$con_array[$hid]="";
+$con_array[$hid]='';
 $con = implode("\n", $con_array);
 file_put_contents($cfile, $con);
 
 
+$re=file($cfile,FILE_SKIP_EMPTY_LINES|FILE_IGNORE_NEW_LINES);
+file_put_contents($cfile,implode("\n",$re));
